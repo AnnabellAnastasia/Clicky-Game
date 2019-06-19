6b1101bdbd8fa79c3ac99ac0c2d71b996a6ba756
Your Name <annabellawilkins@Oss-MacBook-Pro.local>
2019-06-19 18:36:00 -0400

    Updates

---
+++ precache-manifest.11e58722c3147b4f9e244b3b3722f8eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853ebbf1100d9f631d2f737c80d0bede",
+    "url": "/AnnabellAnastasia/Clicky-Game.git/index.html"
+  },
+  {
+    "revision": "d918f56972ccb9be19c3",
+    "url": "/AnnabellAnastasia/Clicky-Game.git/static/css/main.47f79c5c.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/AnnabellAnastasia/Clicky-Game.git/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "d918f56972ccb9be19c3",
+    "url": "/AnnabellAnastasia/Clicky-Game.git/static/js/main.eedc7399.chunk.js"
+  },
+  {
+    "revision": "47f48fb6b90c75caba18",
+    "url": "/AnnabellAnastasia/Clicky-Game.git/static/js/runtime~main.4f05ce39.js"
+  }
+]);
